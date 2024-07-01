a90d684fbeb6000961b471ae9cd52a300a6eba00
Joe Caulfield <joe.caulfield@anza.xyz>
2024-06-30 19:41:48 -0500

    processor: stage feature

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1,18 +1,33 @@
 //! Program state processor
 
 use {
-    crate::{error::FeatureGateError, instruction::FeatureGateInstruction, state::FeatureBitMask},
+    crate::{
+        error::FeatureGateError,
+        instruction::FeatureGateInstruction,
+        state::{
+            collect_staged_features_signer_seeds, get_staged_features_address,
+            get_staged_features_address_and_bump_seed, FeatureBitMask, StagedFeatures,
+        },
+    },
     solana_program::{
         account_info::{next_account_info, AccountInfo},
+        clock::Clock,
         entrypoint::ProgramResult,
         feature::Feature,
         incinerator, msg,
-        program::invoke,
+        program::{invoke, invoke_signed},
         program_error::ProgramError,
         pubkey::Pubkey,
         system_instruction, system_program,
+        sysvar::Sysvar,
     },
 };
+
+// Simply the public key of a keypair created with seed `[0; 32]`, for now.
+const STAGE_AUTHORITY_ADDRESS: Pubkey = Pubkey::new_from_array([
+    59, 106, 39, 188, 206, 182, 164, 45, 98, 163, 168, 208, 42, 111, 13, 115, 101, 50, 21, 119, 29,
+    226, 67, 166, 58, 192, 72, 161, 139, 89, 218, 41,
+]);
 
 /// Processes a [RevokePendingActivation](enum.FeatureGateInstruction.html)
 /// instruction.
@@ -55,9 +70,84 @@ fn process_revoke_pending_activation(
 /// Processes a [StageFeatureForActivation](enum.FeatureGateInstruction.html)
 /// instruction.
 fn process_stage_feature_for_activation(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
 ) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    let feature_info = next_account_info(account_info_iter)?;
+    let staged_features_info = next_account_info(account_info_iter)?;
+    let authority_info = next_account_info(account_info_iter)?;
+
+    // Ensure the proper authority was provided as a signer.
+    if !authority_info.key.eq(&STAGE_AUTHORITY_ADDRESS) {
+        return Err(ProgramError::IncorrectAuthority);
+    }
+    if !authority_info.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    // Ensure the feature exists and has not been activated.
+    // This will also check the owner program ID.
+    if Feature::from_account_info(feature_info)?
+        .activated_at
+        .is_some()
+    {
+        return Err(FeatureGateError::FeatureAlreadyActivated.into());
+    }
+
+    // Load the clock sysvar to get the current epoch.
+    let clock = <Clock as Sysvar>::get()?;
+    let upcoming_epoch = clock.epoch.saturating_add(1);
+
+    // Check to see if the staged features account should be initialized.
+    if staged_features_info.data_is_empty() {
+        let _system_program_info = next_account_info(account_info_iter)?;
+
+        let (staged_features_address, bump_seed) =
+            get_staged_features_address_and_bump_seed(&upcoming_epoch);
+        let bump_seed = [bump_seed];
+        let epoch_as_le = upcoming_epoch.to_le_bytes();
+        let signer_seeds = collect_staged_features_signer_seeds(&epoch_as_le, &bump_seed);
+
+        // Ensure the staged features address is the correct address derived
+        // from the upcoming epoch.
+        if !staged_features_info.key.eq(&staged_features_address) {
+            return Err(FeatureGateError::IncorrectStagedFeaturesAddress.into());
+        }
+
+        // Allocate & assign.
+        invoke_signed(
+            &system_instruction::allocate(
+                &staged_features_address,
+                std::mem::size_of::<StagedFeatures>() as u64,
+            ),
+            &[staged_features_info.clone()],
+            &[&signer_seeds],
+        )?;
+        invoke_signed(
+            &system_instruction::assign(&staged_features_address, program_id),
+            &[staged_features_info.clone()],
+            &[&signer_seeds],
+        )?;
+    } else {
+        // Ensure the staged features address is the correct address derived
+        // from the upcoming epoch.
+        if !staged_features_info
+            .key
+            .eq(&get_staged_features_address(&upcoming_epoch))
+        {
+            return Err(FeatureGateError::IncorrectStagedFeaturesAddress.into());
+        }
+    }
+
+    // Stage the feature. The `stage` method will ensure the feature is not
+    // already staged.
+    let mut staged_features_data = staged_features_info.try_borrow_mut_data()?;
+    bytemuck::try_from_bytes_mut::<StagedFeatures>(&mut staged_features_data)
+        .map_err(|_| ProgramError::InvalidAccountData)
+        .and_then(|staged_features| staged_features.stage(feature_info.key))?;
+
     Ok(())
 }
 
--- program/src/state.rs
+++ program/src/state.rs
@@ -1,7 +1,8 @@
 //! Program state types.
 use {
+    crate::error::FeatureGateError,
     bytemuck::{Pod, Zeroable},
-    solana_program::{clock::Epoch, pubkey::Pubkey},
+    solana_program::{clock::Epoch, program_error::ProgramError, pubkey::Pubkey},
 };
 
 /// The seed prefix (`"staged_features"`) in bytes used to derive the address
@@ -27,7 +28,6 @@ pub(crate) fn collect_staged_features_seeds(epoch_as_le: &[u8; 8]) -> [&[u8]; 2]
     [SEED_PREFIX_STAGED_FEATURES, epoch_as_le]
 }
 
-#[allow(unused)] // For now.
 pub(crate) fn collect_staged_features_signer_seeds<'a>(
     epoch_as_le: &'a [u8; 8],
     bump_seed: &'a [u8],
@@ -66,6 +66,12 @@ pub struct FeatureStake {
     pub stake_support: u64,
 }
 
+impl FeatureStake {
+    fn is_initialized(&self) -> bool {
+        self.feature_id != Pubkey::default()
+    }
+}
+
 /// Features staged for activation at the end of the epoch, with their
 /// corresponding signalled stake support.
 ///
@@ -76,4 +82,83 @@ pub struct StagedFeatures {
     /// Features staged for activation at the end of the epoch, with their
     /// corresponding signalled stake support.
     pub features: [FeatureStake; MAX_FEATURES],
+}
+
+impl StagedFeatures {
+    /// Stage a feature for activation by adding it to the array.
+    pub fn stage(&mut self, feature_id: &Pubkey) -> Result<(), ProgramError> {
+        if self.features.iter().any(|f| &f.feature_id == feature_id) {
+            return Err(FeatureGateError::FeatureAlreadyStaged.into());
+        }
+        if let Some(slot) = self.features.iter_mut().position(|f| !f.is_initialized()) {
+            self.features[slot].feature_id = *feature_id;
+            return Ok(());
+        }
+        Err(FeatureGateError::FeatureStageFull.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_stage(feature_ids: &[Pubkey]) -> StagedFeatures {
+        let mut features = [FeatureStake {
+            feature_id: Pubkey::default(),
+            stake_support: 0,
+        }; MAX_FEATURES];
+
+        for (i, id) in feature_ids.iter().enumerate() {
+            features[i].feature_id = *id;
+        }
+
+        StagedFeatures { features }
+    }
+
+    #[test]
+    fn test_stage_fail_feature_already_staged() {
+        let feature_id = Pubkey::new_unique();
+
+        let mut stage = setup_stage(&[feature_id]);
+
+        assert_eq!(
+            stage.stage(&feature_id).unwrap_err(),
+            FeatureGateError::FeatureAlreadyStaged.into()
+        );
+    }
+
+    #[test]
+    fn test_stage_fail_stage_full() {
+        let feature_id = Pubkey::new_unique();
+
+        let staged_features = vec![Pubkey::new_unique(); MAX_FEATURES];
+        let mut stage = setup_stage(&staged_features);
+
+        assert_eq!(
+            stage.stage(&feature_id).unwrap_err(),
+            FeatureGateError::FeatureStageFull.into()
+        );
+    }
+
+    #[test]
+    fn test_stage_success() {
+        let feature_id = Pubkey::new_unique();
+
+        // Works with an empty stage.
+        let mut stage = setup_stage(&[]);
+        assert_eq!(stage.stage(&feature_id), Ok(()));
+        assert_eq!(stage.features[0].feature_id, feature_id);
+
+        // Works with a partially filled stage.
+        let staged_features = vec![Pubkey::new_unique(); 4];
+        let mut stage = setup_stage(&staged_features);
+        assert_eq!(stage.stage(&feature_id), Ok(()));
+        assert_eq!(stage.features[4].feature_id, feature_id);
+
+        // Works with an almost full stage.
+        let staged_features = vec![Pubkey::new_unique(); MAX_FEATURES - 1];
+        let mut stage = setup_stage(&staged_features);
+        assert_eq!(stage.stage(&feature_id), Ok(()));
+        assert_eq!(stage.features[MAX_FEATURES - 1].feature_id, feature_id);
+    }
 }
