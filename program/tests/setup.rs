82ab69c35b6e398e631c5f7028e6ec0ab41b451c
Joe Caulfield <joe.caulfield@anza.xyz>
2024-08-13 21:09:01 -0400

    SIMD 0072: Add `StageFeatureForActivation` processor tests

--- program/tests/setup.rs
+++ program/tests/setup.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)]
 
 use {
+    solana_feature_gate_program::state::StagedFeatures,
     solana_program_test::*,
     solana_sdk::{
         account::{Account, AccountSharedData},
@@ -41,6 +42,30 @@ pub fn setup_active_feature(context: &mut ProgramTestContext, feature_id: &Pubke
                 1, // `Some`
                 45, 0, 0, 0, 0, 0, 0, 0, // Random slot `u64`
             ],
+            owner: solana_feature_gate_program::id(),
+            ..Account::default()
+        }),
+    );
+}
+
+pub fn setup_stage(feature_ids: &[Pubkey]) -> StagedFeatures {
+    let mut stage = StagedFeatures::default();
+    for (i, id) in feature_ids.iter().enumerate() {
+        stage.features[i].feature_id = *id;
+    }
+    stage
+}
+
+pub fn setup_staged_features_account(
+    context: &mut ProgramTestContext,
+    staged_features_address: &Pubkey,
+    stage: StagedFeatures,
+) {
+    context.set_account(
+        staged_features_address,
+        &AccountSharedData::from(Account {
+            lamports: 100_000_000,
+            data: bytemuck::bytes_of(&stage).to_vec(),
             owner: solana_feature_gate_program::id(),
             ..Account::default()
         }),
