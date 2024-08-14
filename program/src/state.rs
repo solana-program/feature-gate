066e362d6fcf3d3d1c7c5c3c66de8764e6a41db4
Joe Caulfield <joe.caulfield@anza.xyz>
2024-08-13 21:28:33 -0400

    SIMD 0072: Add `SignalSupportForStagedFeatures` state

--- program/src/state.rs
+++ program/src/state.rs
@@ -7,12 +7,19 @@ use {
 
 /// The maximum number of features that can be staged per epoch.
 pub const MAX_FEATURES: usize = 8;
+/// The maxmimum number of historical support signals stored in a Validator
+/// Support Signal account.
+pub const MAX_SIGNALS: usize = 4;
 
 /// The seed prefix (`"staged_features"`) in bytes used to derive the address
 /// of a staged features account.
 /// Seeds: `"staged_features" + epoch`, where epoch is serialized to eight
 /// little-endian bytes.
 pub const SEED_PREFIX_STAGED_FEATURES: &[u8] = b"staged_features";
+/// The seed prefix (`"support_signal"`) in bytes used to derive the address
+/// of a validator support signal account.
+/// Seeds: `"support_signal" + vote_address`.
+pub const SEED_PREFIX_SUPPORT_SIGNAL: &[u8] = b"support_signal";
 
 /// Derive the address of a staged features account.
 pub fn get_staged_features_address(epoch: &Epoch) -> Pubkey {
@@ -29,6 +36,25 @@ pub fn get_staged_features_address_and_bump_seed(epoch: &Epoch) -> (Pubkey, u8) 
 
 pub(crate) fn collect_staged_features_seeds(epoch_as_le: &[u8; 8]) -> [&[u8]; 2] {
     [SEED_PREFIX_STAGED_FEATURES, epoch_as_le]
+}
+
+/// Derive the address of a validator support signal account.
+pub fn get_validator_support_signal_address(vote_account_address: &Pubkey) -> Pubkey {
+    get_validator_support_signal_address_and_bump_seed(vote_account_address).0
+}
+
+/// Derive the address of a validator support signal account, with bump seed.
+pub fn get_validator_support_signal_address_and_bump_seed(
+    vote_account_address: &Pubkey,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &collect_validator_support_signal_seeds(vote_account_address),
+        &crate::id(),
+    )
+}
+
+pub(crate) fn collect_validator_support_signal_seeds(vote_account_address: &Pubkey) -> [&[u8]; 2] {
+    [SEED_PREFIX_SUPPORT_SIGNAL, vote_account_address.as_ref()]
 }
 
 /// A Feature ID and its corresponding stake support, as signalled by
@@ -95,6 +121,26 @@ impl From<&FeatureBitMask> for u8 {
     fn from(mask: &FeatureBitMask) -> u8 {
         mask.0
     }
+}
+
+/// A validator's support signal bitmask along with the epoch the signal
+/// corresponds to.
+#[derive(Clone, Copy, Debug, Default, PartialEq, Pod, Zeroable)]
+#[repr(C)]
+pub struct SupportSignalWithEpoch {
+    /// The epoch the support signal corresponds to.
+    pub epoch: Epoch,
+    /// The support signal bitmask.
+    pub signal: FeatureBitMask,
+    _padding: [u8; 7],
+}
+
+/// A validator's support signal bitmasks with their corresponding epochs.
+#[derive(Clone, Copy, Debug, Default, PartialEq, Pod, Zeroable)]
+#[repr(C)]
+pub struct ValidatorSupportSignal {
+    /// The support signal bitmasks with their corresponding epochs.
+    pub signals: [SupportSignalWithEpoch; MAX_SIGNALS],
 }
 
 #[cfg(test)]
