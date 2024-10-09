1edf4cfbc5bff25933e165e1bac15a1a595826f6
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-09 15:01:10 +0800

    rewrite tests with mollusk

--- program/tests/revoke_pending_activation.rs
+++ program/tests/revoke_pending_activation.rs
@@ -3,185 +3,113 @@
 mod setup;
 
 use {
-    setup::{setup, setup_active_feature, setup_pending_feature},
+    mollusk_svm::{program::keyed_account_for_system_program, result::Check},
+    setup::{active_feature_account, pending_feature_account, setup},
     solana_feature_gate_program::{
         error::FeatureGateError, instruction::revoke_pending_activation,
     },
-    solana_program::instruction::InstructionError,
-    solana_program_test::*,
     solana_sdk::{
-        account::{Account, AccountSharedData},
-        feature::Feature,
+        account::{AccountSharedData, WritableAccount},
+        incinerator,
+        program_error::ProgramError,
         pubkey::Pubkey,
-        signature::{Keypair, Signer},
-        transaction::{Transaction, TransactionError},
     },
 };
 
-#[tokio::test]
-async fn fail_feature_not_signer() {
-    let mut context = setup().start_with_context().await;
+#[test]
+fn fail_feature_not_signer() {
+    let mollusk = setup();
+    let feature = Pubkey::new_unique();
 
-    let mut instruction = revoke_pending_activation(&Pubkey::new_unique());
+    let mut instruction = revoke_pending_activation(&feature);
     instruction.accounts[0].is_signer = false;
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[instruction],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    let error = context
-        .banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-
-    assert_eq!(
-        error,
-        TransactionError::InstructionError(0, InstructionError::MissingRequiredSignature)
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &[
+            (feature, pending_feature_account()),
+            (incinerator::id(), AccountSharedData::default()),
+            keyed_account_for_system_program(),
+        ],
+        &[Check::err(ProgramError::MissingRequiredSignature)],
     );
 }
 
-#[tokio::test]
-async fn fail_feature_incorrect_owner() {
-    let feature_keypair = Keypair::new();
-
-    let mut context = setup().start_with_context().await;
+#[test]
+fn fail_feature_incorrect_owner() {
+    let mollusk = setup();
+    let feature = Pubkey::new_unique();
 
     // Set up a feature account with incorrect owner.
-    {
-        context.set_account(
-            &feature_keypair.pubkey(),
-            &AccountSharedData::from(Account {
-                lamports: 100_000_000,
-                data: vec![0; Feature::size_of()],
-                owner: Pubkey::new_unique(), // Incorrect owner.
-                ..Account::default()
-            }),
-        );
-    }
+    let mut feature_account = pending_feature_account();
+    feature_account.set_owner(Pubkey::new_unique());
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[revoke_pending_activation(&feature_keypair.pubkey())],
-        Some(&context.payer.pubkey()),
-        &[&context.payer, &feature_keypair],
-        context.last_blockhash,
-    );
-
-    let error = context
-        .banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-
-    assert_eq!(
-        error,
-        TransactionError::InstructionError(0, InstructionError::InvalidAccountOwner)
+    mollusk.process_and_validate_instruction(
+        &revoke_pending_activation(&feature),
+        &[
+            (feature, feature_account),
+            (incinerator::id(), AccountSharedData::default()),
+            keyed_account_for_system_program(),
+        ],
+        &[Check::err(ProgramError::InvalidAccountOwner)],
     );
 }
 
-#[tokio::test]
-async fn fail_feature_invalid_data() {
-    let feature_keypair = Keypair::new();
-
-    let mut context = setup().start_with_context().await;
+#[test]
+fn fail_feature_invalid_data() {
+    let mollusk = setup();
+    let feature = Pubkey::new_unique();
 
     // Set up a feature account with invalid data.
-    {
-        context.set_account(
-            &feature_keypair.pubkey(),
-            &AccountSharedData::from(Account {
-                lamports: 100_000_000,
-                data: vec![8; Feature::size_of()],
-                owner: solana_feature_gate_program::id(),
-                ..Account::default()
-            }),
-        );
-    }
+    let mut feature_account = pending_feature_account();
+    feature_account.set_data_from_slice(&[2; 8]);
 
-    let transaction = Transaction::new_signed_with_payer(
-        &[revoke_pending_activation(&feature_keypair.pubkey())],
-        Some(&context.payer.pubkey()),
-        &[&context.payer, &feature_keypair],
-        context.last_blockhash,
-    );
-
-    let error = context
-        .banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-
-    assert_eq!(
-        error,
-        TransactionError::InstructionError(0, InstructionError::InvalidAccountData)
+    mollusk.process_and_validate_instruction(
+        &revoke_pending_activation(&feature),
+        &[
+            (feature, feature_account),
+            (incinerator::id(), AccountSharedData::default()),
+            keyed_account_for_system_program(),
+        ],
+        &[Check::err(ProgramError::InvalidAccountData)],
     );
 }
 
-#[tokio::test]
-async fn fail_feature_already_activated() {
-    let feature_keypair = Keypair::new();
+#[test]
+fn fail_feature_already_activated() {
+    let mollusk = setup();
+    let feature = Pubkey::new_unique();
 
-    let mut context = setup().start_with_context().await;
-
-    // Set up an active feature account.
-    setup_active_feature(&mut context, &feature_keypair.pubkey());
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[revoke_pending_activation(&feature_keypair.pubkey())],
-        Some(&context.payer.pubkey()),
-        &[&context.payer, &feature_keypair],
-        context.last_blockhash,
-    );
-
-    let error = context
-        .banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-
-    assert_eq!(
-        error,
-        TransactionError::InstructionError(
-            0,
-            InstructionError::Custom(FeatureGateError::FeatureAlreadyActivated as u32)
-        )
+    mollusk.process_and_validate_instruction(
+        &revoke_pending_activation(&feature),
+        &[
+            (feature, active_feature_account()),
+            (incinerator::id(), AccountSharedData::default()),
+            keyed_account_for_system_program(),
+        ],
+        &[Check::err(ProgramError::Custom(
+            FeatureGateError::FeatureAlreadyActivated as u32,
+        ))],
     );
 }
 
-#[tokio::test]
-async fn success() {
-    let feature_keypair = Keypair::new();
+#[test]
+fn success() {
+    let mollusk = setup();
+    let feature = Pubkey::new_unique();
 
-    let mut context = setup().start_with_context().await;
-
-    // Set up a pending feature account.
-    setup_pending_feature(&mut context, &feature_keypair.pubkey());
-
-    let transaction = Transaction::new_signed_with_payer(
-        &[revoke_pending_activation(&feature_keypair.pubkey())],
-        Some(&context.payer.pubkey()),
-        &[&context.payer, &feature_keypair],
-        context.last_blockhash,
+    mollusk.process_and_validate_instruction(
+        &revoke_pending_activation(&feature),
+        &[
+            (feature, pending_feature_account()),
+            (incinerator::id(), AccountSharedData::default()),
+            keyed_account_for_system_program(),
+        ],
+        &[
+            Check::success(),
+            Check::compute_units(2_781),
+            // Confirm feature account was closed.
+            Check::account(&feature).closed().build(),
+        ],
     );
-
-    context
-        .banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap();
-
-    // Confirm feature account was closed.
-    let feature_account = context
-        .banks_client
-        .get_account(feature_keypair.pubkey())
-        .await
-        .unwrap();
-    assert!(feature_account.is_none());
 }
